59e51ec82cdc4b7001576563ac70128b36c5671e
ash_weaver <ash@ashes.computer>
2024-08-08 08:39:56 -0700

    minor tweaks

--- admin/admin.js
+++ admin/admin.js
@@ -28,26 +28,6 @@ var formText = `
 // add final stuff to resultText (submit button)
 // apply
 
-
-async function setForm() {
-	document.getElementById("adminBox").innerHTML = formText
-		<form>
-			<label for="name">marker name</label><br>
-			<input type="text" id="name" name="name"><br>
-			<br>
-			<label for="position">marker position</label><br>
-			<input type="text" id="position" name="position"><br>
-			<br>
-			<label for="aliases">search keywords</label><br>
-			<input type="text" id="aliases" name="aliases"><br>
-			<br>
-			<label for="content">content</label><br>
-			<textarea rows="10" cols="50" name="content"></textarea>
-			<hr>
-			<button type="submit">Submit</button>
-		</form>
-`
-
 async function setForm() {
 	document.getElementById("inputBox").innerHTML = formText
 
@@ -103,4 +83,4 @@ async function listMarkers() {
 	}
 	document.getElementById("markersBox").innerHTML = result
 }
-listMarkers()
+listMarkers();
